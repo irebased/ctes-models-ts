6edcd3cfe3ce714f21affb84f3efb17a15c81e2d
rebase <rebased.again@gmail.com>
2026-01-19 16:03:13 -0600

    Create TypeScript model

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ciphertext Ecosystem TypeScript Models
+
+This dependency defines the models for the ciphertext ecosystem, used for building tools that will help solve unsolved ciphers.
+
+## Generating the model
+
+Requires installing the [Protoc CLI](https://protobuf.dev/installation/).
+
+Run `npm install` then `npm run generate` to generate the latest version of the model.
---
+++ generate.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:irebased/CiphertextEcosystemProtobuf.git
+cd CiphertextEcosystemProtobuf
+protoc \
+  --plugin=protoc-gen-ts_proto=../node_modules/.bin/protoc-gen-ts_proto \
+  --ts_proto_out=../ \
+  --ts_proto_opt=esModuleInterop=true,forceLong=string,useOptionals=true \
+  ./model/ciphertext.proto
+cd ..
+mv model/ciphertext.ts index.ts
+rm -rf model
+rm -rf CiphertextEcosystemProtobuf
